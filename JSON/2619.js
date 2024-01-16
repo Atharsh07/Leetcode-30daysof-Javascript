73e885b15667569c017f1bdd686341ee5e91e2d6
Atharsh <atharsh0425@gmail.com>
2024-01-16 20:06:13 +0530

    json

--- JSON/2619.js
+++ JSON/2619.js
@@ -4,3 +4,12 @@ You may assume the array is the output of JSON.parse.
 
   */
 
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length){
+        return this[this.length -1]
+    }
+    return -1
+};
