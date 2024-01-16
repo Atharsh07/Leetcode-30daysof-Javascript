7f290384a66dbff8cb2d733a8cd5f6232c02135d
Atharsh <atharsh0425@gmail.com>
2024-01-16 20:22:46 +0530

    json

--- JSON/2625.js
+++ JSON/2625.js
@@ -8,3 +8,21 @@ Please solve it without the built-in Array.flat method.
 
   */
 
+
+
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n, res=[],curr=0) {
+    for(let i=0;i<arr.length;i++){
+        if(Array.isArray(arr[i])&&curr<n){
+            flat(arr[i],n,res,curr+1)
+        }
+        else{
+            res.push(arr[i])
+        }
+    }
+    return res
+};
